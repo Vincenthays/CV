e511b9b56a5335bc6c00b0a216f93528e23c858c
vincenthays <vincenthays95@gmail.com>
2017-11-21 11:04:16 +0100

    jquery animation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $('header i').click(() => {
     $('body').addClass('nav-toggle');
+    $('nav').animate({ left: 270 }, 400, 'swing');
 });
 
 $('main').click(() => {  
     $('body').removeClass('nav-toggle');
+    $('nav').animate({ left: 0 }, 400, 'swing');
 });
